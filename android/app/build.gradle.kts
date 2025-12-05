ee0c43997736bf73b656ac93ac64e3ebcad756f9
João Dalri <105470028+Dalri05@users.noreply.github.com>
2025-12-05 20:44:10 -0300

    ajuste de build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,14 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
-    // ! NOVO: Plugin do Google Services para o Firebase funcionar
-    id("com.google.gms.google-services")
-}
-
-def keystoreProperties = new Properties()
-def keystorePropertiesFile = rootProject.file('key.properties')
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    // id("com.google.gms.google-services") // Comentado temporariamente para build
 }
 
 android {
@@ -27,33 +20,16 @@ android {
     }
 
     defaultConfig {
-        // TODO: Se você mudou o ID no Firebase, mude aqui também.
         applicationId = "com.example.car_store"
-
-        // ! MUDANÇA: Mudei de flutter.minSdkVersion para 23 para evitar erros do Firebase
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    // ! NOVO: Configuração da Assinatura (Lendo do key.properties)
-    signingConfigs {
-        release {
-            keyAlias = keystoreProperties['keyAlias']
-            keyPassword = keystoreProperties['keyPassword']
-            storeFile = keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-            storePassword = keystoreProperties['storePassword']
-        }
-    }
-
     buildTypes {
         release {
-            // ! MUDANÇA: Usando a configuração 'release' que criamos acima
-            signingConfig = signingConfigs.release
-            // Configurações para manter o código legível e evitar erros na versão release
-            minifyEnabled false
-            shrinkResources false
+            // Usando debug signing para facilitar
         }
     }
 }
